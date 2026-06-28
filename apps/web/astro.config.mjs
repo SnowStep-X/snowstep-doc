6164b27d3cbf577a45b9ac77d51da0e381e7d029
SnowStep <snowstep@local>
2026-06-28 23:27:23 +0800

    chore(web): scaffold Astro app

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  site: 'https://snowstep-doc.vercel.app',
+  output: 'static',
+});
+
